92194a5fc9fc179d7500f95b977074b4786ed633
Berat Arpacı <beroarpc@gmmail.com>
2025-06-05 13:55:54 +0300

    Dashboard default export olarak düzeltildi

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,7 +2,7 @@
 
 import React from "react";
 
-export function Dashboard() {
+export default function Dashboard() {
   return (
     <div className="p-6">
       <h1 className="text-3xl font-bold mb-4">Teftiş Dashboard</h1>
