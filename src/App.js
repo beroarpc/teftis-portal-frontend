ec2b8d3d5e679025fffc7edf1b8a4addda09da7e
Berat Arpacı <beroarpc@gmmail.com>
2025-06-06 19:44:14 +0300

    App.js import fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { Login } from "./pages/Login";
-import { Dashboard } from "./pages/Dashboard";
+import Dashboard from "./pages/Dashboard"; 
 
 function App() {
   const token = localStorage.getItem("token");
